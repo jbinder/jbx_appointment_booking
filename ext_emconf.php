15f8f71a3c08aca721d4e68b4376bc58cd871152
Johannes Binder <j.binder.x@gmail.com>
2011-08-19 16:25:57 +0200

    bare skeleton added

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "appointment_booking".
+#
+# Auto generated 19-08-2011 16:02
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Appointment Booking',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Johannes Binder',
+	'author_email' => 'j.binder.x@gmail.com',
+	'shy' => '',
+	'dependencies' => 'smarty',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'smarty' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"6b95";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5c89";s:14:"ext_tables.php";s:4:"2689";s:16:"locallang_db.xml";s:4:"efaa";s:19:"doc/wizard_form.dat";s:4:"4aef";s:20:"doc/wizard_form.html";s:4:"4b53";s:39:"pi1/class.tx_appointmentbooking_pi1.php";s:4:"b213";s:17:"pi1/locallang.xml";s:4:"2be8";}',
+);
+
+?>
